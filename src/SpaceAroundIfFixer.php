863b32cd31c519e44702e7a8fd20c29c5783e52b
Andrea Gandino <andreagandino@gmail.com>
2024-08-17 10:07:58 +0200

    Update

---
+++ src/SpaceAroundIfFixer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SuperDJ\SpacesInParenthesesFixer;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+class SpaceAroundIfFixer extends AbstractFixer
+{
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new \PhpCsFixer\FixerDefinition\FixerDefinition(
+            'Ensure there are spaces around the "if" token.',
+            []
+        );
+    }
+
+    public function getName(): string
+    {
+        return 'Custom/space_around_if';
+    }
+
+    public function isCandidate(Tokens $tokens):bool
+    {
+        return $tokens->isTokenKindFound(T_IF);
+    }
+
+    public function applyFix(\SplFileInfo $file, Tokens $tokens):void
+    {
+        foreach ($tokens as $index => $token) {
+            if ($token->isGivenKind(T_IF)) {
+                // Add a space before 'if' if not present
+                if (!$tokens[$index - 1]->isWhitespace()) {
+                    $tokens->insertAt($index, new Token([T_WHITESPACE, ' ']));
+                }
+
+                // Add a space after 'if' if not present
+                if (!$tokens[$index + 1]->isWhitespace()) {
+                    $tokens->insertAt($index + 1, new Token([T_WHITESPACE, ' ']));
+                }
+            }
+        }
+    }
+}
