305d4383fbe39f7b5568a61ce21c88214f68f044
Andrea Gandino <andreagandino@gmail.com>
2024-08-17 12:28:52 +0200

    Update

--- src/SpaceAroundIfFixer.php
+++ src/SpaceAroundIfFixer.php
@@ -9,38 +9,38 @@ use PhpCsFixer\Tokenizer\Tokens;
 
 class SpaceAroundIfFixer extends AbstractFixer
 {
-    public function getDefinition(): FixerDefinitionInterface
-    {
-        return new \PhpCsFixer\FixerDefinition\FixerDefinition(
-            'Ensure there are spaces around the "if" token.',
-            []
-        );
-    }
+	public function getDefinition(): FixerDefinitionInterface
+	{
+		return new \PhpCsFixer\FixerDefinition\FixerDefinition(
+			'Ensure there are spaces around the "if" token.',
+			[]
+		);
+	}
 
-    public function getName(): string
-    {
-        return 'Custom/space_around_if';
-    }
+	public function getName(): string
+	{
+		return 'Andreg/space_around_if';
+	}
 
-    public function isCandidate(Tokens $tokens):bool
-    {
-        return $tokens->isTokenKindFound(T_IF);
-    }
+	public function isCandidate(Tokens $tokens):bool
+	{
+		return $tokens->isTokenKindFound(T_IF);
+	}
 
-    public function applyFix(\SplFileInfo $file, Tokens $tokens):void
-    {
-        foreach ($tokens as $index => $token) {
-            if ($token->isGivenKind(T_IF)) {
-                // Add a space before 'if' if not present
-                if (!$tokens[$index - 1]->isWhitespace()) {
-                    $tokens->insertAt($index, new Token([T_WHITESPACE, ' ']));
-                }
+	public function applyFix(\SplFileInfo $file, Tokens $tokens):void
+	{
+		foreach ($tokens as $index => $token) {
+			if ($token->isGivenKind(T_IF)) {
+				// Add a space before 'if' if not present
+				if (!$tokens[$index - 1]->isWhitespace()) {
+					$tokens->insertAt($index, new Token([T_WHITESPACE, ' ']));
+				}
 
-                // Add a space after 'if' if not present
-                if (!$tokens[$index + 1]->isWhitespace()) {
-                    $tokens->insertAt($index + 1, new Token([T_WHITESPACE, ' ']));
-                }
-            }
-        }
-    }
+				// Add a space after 'if' if not present
+				if (!$tokens[$index + 1]->isWhitespace()) {
+					$tokens->insertAt($index + 1, new Token([T_WHITESPACE, ' ']));
+				}
+			}
+		}
+	}
 }
