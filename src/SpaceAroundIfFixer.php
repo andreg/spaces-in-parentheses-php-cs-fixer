18d5583d763c92fe1029d1eaf34fcf08fd98acda
Andrea Gandino <andreagandino@gmail.com>
2024-08-17 12:33:24 +0200

    Update

--- src/SpaceAroundIfFixer.php
+++ src/SpaceAroundIfFixer.php
@@ -24,13 +24,13 @@ class SpaceAroundIfFixer extends AbstractFixer
 
 	public function isCandidate(Tokens $tokens):bool
 	{
-		return $tokens->isTokenKindFound(T_IF);
+		return $tokens->isTokenKindFound(T_IF) || $tokens->isTokenKindFound(T_CATCH);
 	}
 
 	public function applyFix(\SplFileInfo $file, Tokens $tokens):void
 	{
 		foreach ($tokens as $index => $token) {
-			if ($token->isGivenKind(T_IF)) {
+			if ($token->isGivenKind(T_IF) || $token->isGivenKind(T_CATCH)) {
 				// Add a space before 'if' if not present
 				if (!$tokens[$index - 1]->isWhitespace()) {
 					$tokens->insertAt($index, new Token([T_WHITESPACE, ' ']));
