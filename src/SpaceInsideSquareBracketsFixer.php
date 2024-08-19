b0e774e2a6e19aa524772e7ea1ac22d0cffeeacd
Andrea Gandino <andreagandino@gmail.com>
2024-08-19 15:01:05 +0200

    Update

---
+++ src/SpaceInsideSquareBracketsFixer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SuperDJ\SpacesInParenthesesFixer;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+class SpaceInsideSquareBracketsFixer extends AbstractFixer
+{
+	public function getDefinition(): FixerDefinitionInterface
+	{
+		return new \PhpCsFixer\FixerDefinition\FixerDefinition(
+			'Ensure there are spaces inside square brackets token.',
+			[]
+		);
+	}
+
+	public function getName(): string
+	{
+		return 'Andreg/space_inside_square_brackets';
+	}
+
+	public function isCandidate(Tokens $tokens):bool
+	{
+		return $tokens->isTokenKindFound('[') || $tokens->isTokenKindFound(']');
+	}
+
+	public function applyFix(\SplFileInfo $file, Tokens $tokens):void
+	{
+		foreach ($tokens as $index => $token) {
+			if ( ! $token->equals( '[' ) && ! $token->equals( ']' ) ) {
+				continue;
+			}
+
+			if ( $token->equals( '[' ) ) {
+				if (!$tokens[$index + 1]->isWhitespace()) {
+					$tokens->insertAt($index + 1, new Token([T_WHITESPACE, ' ']));
+				}
+			}
+
+			if ( $token->equals( ']' ) ) {
+				if (!$tokens[$index - 1]->isWhitespace()) {
+					$tokens->insertAt($index, new Token([T_WHITESPACE, ' ']));
+				}
+			}
+		}
+	}
+}
