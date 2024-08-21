1a2a9af589885d6dd67fb29e4794124b177c40ca
Andrea Gandino <andreagandino@gmail.com>
2024-08-21 09:31:48 +0200

    Update

--- src/SpaceInsideRoundBracketsFixer.php
+++ src/SpaceInsideRoundBracketsFixer.php
@@ -7,44 +7,53 @@ use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 
-class SpaceInsideSquareBracketsFixer extends AbstractFixer
+class SpaceInsideRoundBracketsFixer extends AbstractFixer
 {
-	public function getDefinition(): FixerDefinitionInterface
-	{
-		return new \PhpCsFixer\FixerDefinition\FixerDefinition(
-			'Ensure there are spaces inside square brackets token.',
-			[]
-		);
-	}
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new \PhpCsFixer\FixerDefinition\FixerDefinition(
+            "Ensure there are spaces inside round brackets token.",
+            []
+        );
+    }
 
-	public function getName(): string
-	{
-		return 'Andreg/space_inside_square_brackets';
-	}
+    public function getName(): string
+    {
+        return "Andreg/space_inside_round_brackets";
+    }
 
-	public function isCandidate(Tokens $tokens):bool
-	{
-		return $tokens->isTokenKindFound('[') || $tokens->isTokenKindFound(']');
-	}
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isTokenKindFound("(") || $tokens->isTokenKindFound(")");
+    }
 
-	public function applyFix(\SplFileInfo $file, Tokens $tokens):void
-	{
-		foreach ($tokens as $index => $token) {
-			if ( ! $token->equals( '[' ) && ! $token->equals( ']' ) ) {
-				continue;
-			}
+    public function applyFix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token) {
+            if (!$token->equals("(") && !$token->equals(")")) {
+                continue;
+            }
 
-			if ( $token->equals( '[' ) ) {
-				if (!$tokens[$index + 1]->isWhitespace() && !$tokens[$index + 1]->equals( ']' )) {
-					$tokens->insertAt($index + 1, new Token([T_WHITESPACE, ' ']));
-				}
-			}
+            if ($token->equals("(")) {
+                if (
+                    !$tokens[$index + 1]->isWhitespace() &&
+                    !$tokens[$index + 1]->equals(")")
+                ) {
+                    $tokens->insertAt(
+                        $index + 1,
+                        new Token([T_WHITESPACE, " "])
+                    );
+                }
+            }
 
-			if ( $token->equals( ']' ) ) {
-				if (!$tokens[$index - 1]->isWhitespace() && !$tokens[$index - 1]->equals( '[' )) {
-					$tokens->insertAt($index, new Token([T_WHITESPACE, ' ']));
-				}
-			}
-		}
-	}
+            if ($token->equals(")")) {
+                if (
+                    !$tokens[$index - 1]->isWhitespace() &&
+                    !$tokens[$index - 1]->equals("(")
+                ) {
+                    $tokens->insertAt($index, new Token([T_WHITESPACE, " "]));
+                }
+            }
+        }
+    }
 }
